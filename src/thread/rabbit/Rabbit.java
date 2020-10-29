19e061c99b7b21d36bdfa9e334a104ac4d45d816
divyanshgarg01 <divyansh.garg_cs19@gla.ac.in>
2020-10-29 20:31:36 +0530

    add a private field for rabbit and getter and setter methods for it.

--- src/thread/rabbit/Rabbit.java
+++ src/thread/rabbit/Rabbit.java
@@ -5,8 +5,8 @@ import javax.swing.*;
 public class Rabbit implements Runnable {
 
     private static final int MILESTONES = 5;
-
     private final Thread rabbit;
+    private float timeUsedByRabbit = 0f;
 
     public Rabbit() {
         this.rabbit = new Thread(this, "Rabbit");
@@ -16,30 +16,44 @@ public class Rabbit implements Runnable {
         return rabbit;
     }
 
+    public float getTimeUsedByRabbit() {
+        return timeUsedByRabbit;
+    }
+
+    public void setTimeUsedByRabbit(float timeUsedByRabbit) {
+        this.timeUsedByRabbit = timeUsedByRabbit;
+    }
+
     @Override
     public void run() {
         for (int index = 0; index < MILESTONES; index++) {
             switch (index) {
                 case 0:
                     System.out.println("The Rabbit has started from the start line!");
+                    timeUsedByRabbit = (float) (timeUsedByRabbit + 1.5);
                     break;
                 case 1:
                     System.out.println("The Rabbit has reached the Cool River!");
+                    timeUsedByRabbit = (float) (timeUsedByRabbit + 1.5);
                     break;
                 case 2:
                     System.out.println("The Rabbit has reached the Mountain Hill!");
+                    timeUsedByRabbit = (float) (timeUsedByRabbit + 1.5);
                     break;
                 case 3:
                     System.out.println("The Rabbit has reached the Big Oak Tree!");
                     System.out.println("The Rabbit is leading and hence is going to sleep!");
+                    timeUsedByRabbit = (float) (timeUsedByRabbit + 1.5);
                     try {
                         Thread.sleep(9000L);
                     } catch (InterruptedException e) {
                         System.err.println("The Rabbit's sleep got interrupted!");
                     }
+                    timeUsedByRabbit = (float) (timeUsedByRabbit + 9.0);
                     break;
                 case 4:
                     System.out.println("The Rabbit has reached the finish line!");
+                    timeUsedByRabbit = (float) (timeUsedByRabbit + 1.5);
                     break;
             }
             try {
