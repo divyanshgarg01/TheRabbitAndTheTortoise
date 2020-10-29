6f3b404935f588ca3fcc36939d325a79681ba85d
divyanshgarg01 <divyansh.garg_cs19@gla.ac.in>
2020-10-29 21:14:06 +0530

    add the execution for the race.

--- src/main/Race.java
+++ src/main/Race.java
@@ -1,10 +1,30 @@
 package main;
 
 import thread.rabbit.Rabbit;
+import thread.tortoise.Tortoise;
+
+import javax.swing.*;
+
+import static java.lang.Thread.sleep;
 
 public class Race {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Rabbit rabbit = new Rabbit();
         rabbit.getRabbit().start();
+        Tortoise tortoise = new Tortoise();
+        tortoise.getTortoise().start();
+        String winner = null;
+        sleep(25000L);
+        {
+            if (tortoise.getTimeUsedByTortoise() < rabbit.getTimeUsedByRabbit()) {
+                winner = "Tortoise wins the race";
+            } else if (tortoise.getTimeUsedByTortoise() == rabbit.getTimeUsedByRabbit()) {
+                winner = "No one wins the race";
+            } else {
+                winner = "Rabbit wins the race";
+            }
+            JOptionPane.showMessageDialog(null, winner);
+        }
     }
 }
+
--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -1,14 +1,16 @@
 package thread.tortoise;
 
-public class Tortoise extends Thread{
+import javax.swing.*;
+
+public class Tortoise extends Thread {
 
     private static final int MILESTONES = 5;
 
     private final Thread tortoise;
     private float timeUsedByTortoise = 0f;
 
-    public Tortoise(Thread tortoise) {
-        this.tortoise = tortoise;
+    public Tortoise() {
+        this.tortoise = new Thread(this, "tortoise");
     }
 
     public Thread getTortoise() {
@@ -21,5 +23,44 @@ public class Tortoise extends Thread{
 
     public void setTimeUsedByTortoise(float timeUsedByTortoise) {
         this.timeUsedByTortoise = timeUsedByTortoise;
+    }
+
+    @Override
+    public void run() {
+        for (int index = 0; index < MILESTONES; index++) {
+            switch (index) {
+                case 0:
+                    System.out.println("The Tortoise has started from the start line!");
+                    timeUsedByTortoise = (float) (timeUsedByTortoise + 3.0);
+                    break;
+                case 1:
+                    System.out.println("The Tortoise has reached the Cool River!");
+                    timeUsedByTortoise = (float) (timeUsedByTortoise + 3.0);
+                    break;
+                case 2:
+                    System.out.println("The Tortoise has reached the Mountain Hill!");
+                    timeUsedByTortoise = (float) (timeUsedByTortoise + 3.0);
+                    break;
+                case 3:
+                    System.out.println("The tortoise has reached the Big Oak Tree!");
+                    timeUsedByTortoise = (float) (timeUsedByTortoise + 3.0);
+                    break;
+                case 4:
+                    System.out.println("The Tortoise has reached the finish line!");
+                    timeUsedByTortoise = (float) (timeUsedByTortoise + 3.0);
+                    break;
+            }
+            try {
+                Thread.sleep(3000L);
+            } catch (InterruptedException e) {
+                System.err.println("The tortoise has lost the path!");
+            }
+        }
+        JOptionPane.showConfirmDialog(
+                null,
+                "The Tortoise has completed the race!",
+                "Tortoise",
+                JOptionPane.OK_CANCEL_OPTION
+        );
     }
 }
