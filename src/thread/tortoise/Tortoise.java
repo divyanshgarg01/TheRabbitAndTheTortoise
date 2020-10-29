e96f4f1aade9dc55745c7916ccc26ae4353116dc
divyanshgarg01 <divyansh.garg_cs19@gla.ac.in>
2020-10-29 20:39:29 +0530

    add getter and setter methods for private field.

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -5,9 +5,21 @@ public class Tortoise extends Thread{
     private static final int MILESTONES = 5;
 
     private final Thread tortoise;
-    private float timeTakenByTortoiseToCompleteTheRace = 0f;
+    private float timeUsedByTortoise = 0f;
 
     public Tortoise(Thread tortoise) {
         this.tortoise = tortoise;
+    }
+
+    public Thread getTortoise() {
+        return tortoise;
+    }
+
+    public float getTimeUsedByTortoise() {
+        return timeUsedByTortoise;
+    }
+
+    public void setTimeUsedByTortoise(float timeUsedByTortoise) {
+        this.timeUsedByTortoise = timeUsedByTortoise;
     }
 }
