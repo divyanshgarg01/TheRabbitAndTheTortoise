3e6243a02abbea606c39ec298b0be14c0629e83e
divyanshgarg01 <divyansh.garg_cs19@gla.ac.in>
2020-10-29 20:37:35 +0530

    add constructor method for thread field.

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -1,9 +1,13 @@
 package thread.tortoise;
 
-import javax.swing.*;
-
 public class Tortoise extends Thread{
 
     private static final int MILESTONES = 5;
 
+    private final Thread tortoise;
+    private float timeTakenByTortoiseToCompleteTheRace = 0f;
+
+    public Tortoise(Thread tortoise) {
+        this.tortoise = tortoise;
+    }
 }
